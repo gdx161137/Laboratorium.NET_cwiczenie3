194c506715bcbe36ce712147df30ba3da8bff8ac
Jakub Pacler <jakub.pacler@gmail.com>
2024-04-05 17:48:32 +0200

    Add project files.

---
+++ Laboratorium.NET_cwiczenie3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorium.NET_cwiczenie3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                string lokalizacja = "C:\\aaMag\\test.txt";
+
+                if (File.Exists(lokalizacja))
+                {
+                using (FileStream fs = new FileStream(lokalizacja, FileMode.Open))
+                    {
+                        using (StreamReader reader = new StreamReader(fs))
+                        {
+                            Console.WriteLine("Zawartość pliku:");
+                            Console.WriteLine("*****");
+                            Console.WriteLine(reader.ReadToEnd());
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Podany plik nie istnieje.");
+                }
+
+            Console.ReadKey();
+        }
+    }
+}
